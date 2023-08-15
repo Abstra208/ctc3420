dfd8cf522cd819f0154c4361b249e7c75a2a39e3
Ludovic Morin <LudovicMorin357@hotmail.com>
2023-08-15 00:19:28 +0000

    modification de code pour importer le stockage firebase

--- js/app.js
+++ js/app.js
@@ -1,18 +1,17 @@
-import { initializeApp } from 'firebase/app';
-import { getStorage, ref, uploadBytes } from "firebase/storage";
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js';
+import { getStorage, ref, uploadBytes } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-storage.js";
 
-const firebaseConfig = {
+const firebaseApp = initializeApp({
     apiKey: "AIzaSyDZOojkQJf0PIXfYgMf6SdyurI22vEjnnk",
     authDomain: "ctc3420.firebaseapp.com",
     projectId: "ctc3420",
     storageBucket: "ctc3420.appspot.com",
     messagingSenderId: "298220775488",
     appId: "1:298220775488:web:b24600577123d5946a97a5",
-    measurementId: "G-27E2ZLHJW0"
-};
+    measurementId: "G-27E2ZLHJW0"    
+});
+const storage = getStorage(firebaseApp);
 
-const app = initializeApp(firebaseConfig);
-const storage = getStorage(app);
 
 const telechargerBtn = document.getElementById("telecharger");
 telechargerBtn.addEventListener("click", () => {
