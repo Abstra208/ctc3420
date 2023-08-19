8b238fae1b41645a5f428577ebca765c97673116
Ludovic Morin <LudovicMorin357@hotmail.com>
2023-08-19 01:50:45 +0000

    changement du pathname

--- js/header.js
+++ js/header.js
@@ -2,7 +2,7 @@ link = {
     "horaires": "",
     "nouveau": "",
 }
-links = [["/nouveau", "new"], ["/", "home"]]
+links = [["/nouveau", "new"], ["/", "accueil"]]
 
 links.forEach(element => {
     if (element[0] == location.pathname) {
